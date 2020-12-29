10f8f737392d992bfff65fa722ef7c76bdbb6af0
Krzysztof Talar <krzysztoftalar@protonmail.com>
2020-12-29 13:37:07 +0100

    Data updated

--- gatsby-app/src/components/homePage/HomeContent.tsx
+++ gatsby-app/src/components/homePage/HomeContent.tsx
@@ -16,7 +16,7 @@ const HomeContent = (): JSX.Element => {
         >
             <Content>
                 Hi, I&apos;m Krzysztof Talar. <br />
-                I&apos;m Full-Stack Developer, React and .NET.
+                I&apos;m a Full-Stack Developer, React, Flutter, and .NET.
             </Content>
         </HomeContentSection>
     );
