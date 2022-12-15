024ed596d3b4fc6596de05c5a4b7dc47386858ac
Krzysztof Talar <krzysztofTalar@protonmail.com>
2022-12-15 14:55:48 +0100

    [Task] Moved cdnjs links for react-slick to gatsby-ssr.js

--- gatsby-app/src/components/ui/Carousel.tsx
+++ gatsby-app/src/components/ui/Carousel.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
-import Slider from 'react-slick';
+import Slider, { Settings } from 'react-slick';
+
 import '../../styles/components/carousel.css';
 
-const settings = {
+const settings: Settings = {
     autoplay: true,
     autoplaySpeed: 10000,
     speed: 500,
@@ -19,23 +19,7 @@ interface IProps {
 }
 
 const Carousel: React.FC<IProps> = ({ children }) => {
-    return (
-        <>
-            <Helmet>
-                <link
-                    rel="stylesheet"
-                    type="text/css"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-                />
-                <link
-                    rel="stylesheet"
-                    type="text/css"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-                />
-            </Helmet>
-            <Slider {...settings}>{children}</Slider>
-        </>
-    );
+    return <Slider {...settings}>{children}</Slider>;
 };
 
 export default Carousel;
